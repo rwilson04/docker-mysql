06b089236c493f063c82c6ca6499eda5b0cced90
Reese Wilson <reese@shinymayhem.com>
2015-02-05 05:11:58 +0000

    updating init.sh to use :data tag

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ read -s -p "MySQL Nodify Password:" nodifypw
 docker run \
 	--name mysql-data \
 	-e MYSQL_PASS=$nodifypw \
-	shinymayhem/mysql bash '/init-db.sh'
+	shinymayhem/mysql:data bash '/init-db.sh'
 	#/etc/init.d/mysql start && \
 	#mysql -u nodify -p$nodifypw nodify < sql/zf-commons.sql && \
 	#mysql -u nodify -p$nodifypw nodify < sql/scn-social-auth
