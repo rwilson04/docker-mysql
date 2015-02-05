65603a1faceb7206f355bd8ae2ed5153a5773945
Reese Wilson <reese@shinymayhem.com>
2015-02-05 04:56:57 +0000

    removing surrounding quotes from env

--- init.sh
+++ init.sh
@@ -2,10 +2,7 @@ read -s -p "MySQL Nodify Password:" nodifypw
 	#-v /var/lib/mysql \
 docker run \
 	--name mysql-data \
-	-e MYSQL_USER="nodify-web" \
 	-e MYSQL_PASS=$nodifypw \
-	-e MYSQL_DATABASE="nodify" \
-	-e STARTUP_SQL="/sql/node.sql /sql/zf-commons.sql /sql/scn-social-auth.sql" \
 	shinymayhem/mysql bash '/init-db.sh'
 	#/etc/init.d/mysql start && \
 	#mysql -u nodify -p$nodifypw nodify < sql/zf-commons.sql && \
